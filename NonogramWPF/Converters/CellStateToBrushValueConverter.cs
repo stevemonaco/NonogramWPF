7c89cf835770222cded83379ff5e5013c36d4e9d
Stephen Monaco <stevemonaco@hotmail.com>
2018-10-14 15:00:40 -0400

    Corrected grid cell binded Background updating

--- NonogramWPF/Converters/CellStateToBrushValueConverter.cs
+++ NonogramWPF/Converters/CellStateToBrushValueConverter.cs
@@ -8,32 +8,32 @@ using NonogramWPF.Model;
 
 namespace NonogramWPF.Converters
 {
-    class NonogramCellToBrushValueConverter : IValueConverter
+    class CellStateToBrushValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if(value is null)
                 throw new ArgumentNullException(nameof(value));
 
-            if(!(value is NonogramCell))
+            if(!(value is CellState))
                 throw new ArgumentException($"Cannot convert from {value.GetType()} to {typeof(NonogramCell)}", nameof(value));
 
             if (!targetType.IsAssignableFrom(typeof(Brush)))
                 throw new ArgumentException($"Cannot convert from {typeof(Brush)} to {targetType}", nameof(targetType));
 
-            if(value is NonogramCell cell)
+            if(value is CellState state)
             {
-                if (cell.CellState == CellState.Undetermined)
+                if (state == CellState.Undetermined)
                     return new SolidColorBrush(Colors.Gray);
-                else if (cell.CellState == CellState.Empty)
+                else if (state == CellState.Empty)
                     return new SolidColorBrush(Colors.Aqua);
-                else if (cell.CellState == CellState.Filled)
+                else if (state == CellState.Filled)
                     return new SolidColorBrush(Colors.Black);
                 else
-                    throw new ArgumentException($"Unhandled case of {nameof(NonogramCell)}");
+                    throw new ArgumentException($"Unhandled case of {nameof(CellState)}");
             }
             else
-                throw new ArgumentException($"Cannot convert from {value.GetType()} to {typeof(NonogramCell)}", nameof(value));
+                throw new ArgumentException($"Cannot convert from {value.GetType()} to {typeof(CellState)}", nameof(value));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
